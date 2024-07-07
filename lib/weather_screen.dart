358f30d7bb6cf999d7792f7fa8a84c60cb0d6631
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-07 23:29:23 +0530

    Update weather_screen.dart

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,10 +1,117 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Weather App',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  elevation: 10,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "300 F",
+                              style: TextStyle(
+                                  fontSize: 32, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Icon(Icons.cloud, size: 64),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Text("Rain", style: TextStyle(fontSize: 20))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Weather Forecast",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      elevation: 6,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "09:00",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Icon(
+                              Icons.cloud,
+                              size: 32,
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text("300 F"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const Placeholder(fallbackHeight: 150),
+              const SizedBox(
+                height: 20,
+              ),
+              const Placeholder(fallbackHeight: 150),
+            ],
+          ),
+        ));
   }
 }
